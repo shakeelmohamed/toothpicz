d65c4e7e1739fc1b78fa5ec310f2ea3d745dd0b6
Shakeel Mohamed <Shakeel_mohamed@live.com>
2012-11-04 16:53:58 -0800

    added postgresql connection to index.php

--- LAMP/index.php
+++ LAMP/index.php
@@ -1,3 +1,11 @@
 <?php
-	echo 'Welcome to ToothPicz';
+	//Here is the basic DB connection
+	$dbconn = pg_connect("host=ec2-54-243-62-232.compute-1.amazonaws.com port=5432 dbname=d482hpig0og3o7 user=eprbfwldrxxxsq password=tcZtq8ZRT6KTs32vSD2ctHObso sslmode=require options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error());
+
+
+	echo 'Welcome to ToothPicz <br />';
+	if($dbconn)
+		echo 'DB connection worked!';
+	else
+		echo 'DB connection failed!';
 ?>
