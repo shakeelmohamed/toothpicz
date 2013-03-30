cbf01507b65fad4bacf958e593cc45a887c53ef3
Andrew Craswell <retype_it@yahoo.com>
2013-03-30 12:27:22 -0700

    Added initial website template.

---
+++ Website/ToothPicz/ToothPicz/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace ToothPicz
+{
+public class BundleConfig
+	{
+		// For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			
+		}
+	}
+}
